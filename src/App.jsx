0d1692265ed41d87cceceb70d51b7e2c1d38c38b
keerthanasalapaka <keerthana.salapaka@gmail.com>
2024-06-27 19:11:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((count) => {
+      return count + 1;
+    });
+  };
+
+  const handleDecrement = () => {
+    setCount((count) => {
+      return count - 1;
+    });
+  };
+  return (
+    <>
+      <div>
+        <h3>count:{count}</h3>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
